c5510a21a570bf2f179bce0487a318725b52e03a
Logan Smith] <ls1130343@gmail.com>
2025-09-19 00:30:57 -0500

    Merge with Main

--- src/main.py
+++ src/main.py
@@ -7,17 +7,7 @@ def setup_curses(stdscr):
     stdscr.keypad(True)
     curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
     curses.mouseinterval(150)
-import curses
-from src.classes import GameStatus, GameManager
-from src.tui.run_tui import Frontend
 
-def setup_curses(stdscr):
-    curses.curs_set(0)
-    stdscr.keypad(True)
-    curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
-    curses.mouseinterval(150)
-
-def main(stdscr):
 def main(stdscr):
     ### Initialize
 
@@ -30,18 +20,6 @@ def main(stdscr):
     
     stdscr.refresh()
     frontend.start_game()
-
-
-    # Show start screen and get mine count
-
-    setup_curses(stdscr)
-
-    frontend = Frontend(stdscr)
-    frontend.set_num_mines()
-    
-    stdscr.refresh()
-    frontend.start_game()
-
 
 if __name__ == "__main__":
     curses.wrapper(main)
